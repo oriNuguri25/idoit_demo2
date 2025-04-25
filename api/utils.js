4000440947554994ab43cb1d6b261a1c9ab6311b
oriNuguri25 <byeongjun25@naver.com>
2025-04-25 14:30:34 +0900

    10차 수정

--- api/utils.js
+++ api/utils.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 // Supabase 클라이언트 초기화
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // CORS 헤더 설정 함수
